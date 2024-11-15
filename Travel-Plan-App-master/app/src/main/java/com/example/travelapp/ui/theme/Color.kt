3b169ece23b3e600b3fe0f60eb563d3288139f72
surenth14 <165827586+surenth14@users.noreply.github.com>
2024-11-15 22:31:59 +0530

    Add files via upload

---
+++ Travel-Plan-App-master/app/src/main/java/com/example/travelapp/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.travelapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF00EE28)
+val Purple700 = Color(0xFFB30000)
+val Teal200 = Color(0xFF03DAC5)
